submmission moving average v2 fix

bot/policies/moving_average - Copy.py
@@ -18,12 +18,12 @@ class VerySimplePolicy(Policy):
         self.price_history = deque(maxlen=window_size)
 
         self.max_price_history.append(0)
-        self.min_price_history.append(0)
+        self.min_price_history.append(100)
         self.price_history.append(0)
 
 # 6-6-6
-# Average profit ($): 206.81 ± 92.66
-# Average profit inc rundown ($): 328.84
+# Average profit ($): 203.44 ± 96.61
+# Average profit inc rundown ($): 343.25
 
     def act(self, external_state, internal_state):
         market_price = external_state['price']
@@ -31,7 +31,7 @@ class VerySimplePolicy(Policy):
         min_moving_average = np.mean(self.min_price_history)
         moving_average = np.mean(self.price_history)
         # print(f'Max: {max_moving_average}, Min: {min_moving_average}, Average: {moving_average}')
-        if market_price > max_moving_average:
+        if max_moving_average < market_price:
             self.max_price_history.append(market_price)
             # print(np.mean(self.max_price_history))
         if market_price < min_moving_average:
@@ -40,7 +40,6 @@ class VerySimplePolicy(Policy):
         self.price_history.append(market_price)
 
         if len(self.max_price_history) == len(self.min_price_history) == self.window_size:
-
             if market_price > max_moving_average:
                 charge_kW = -internal_state['max_charge_rate']
                 solar_kW_to_battery = 0
@@ -50,9 +49,11 @@ class VerySimplePolicy(Policy):
             elif market_price > moving_average:
                 charge_kW = -internal_state['max_charge_rate']
                 solar_kW_to_battery = 0
+                # self.max_price_history.append(moving_average * 2)
             else:
                 charge_kW = internal_state['max_charge_rate']
                 solar_kW_to_battery = external_state['pv_power']
+                # self.min_price_history.append(moving_average / 2)
         else:
             solar_kW_to_battery = charge_kW = 0
 