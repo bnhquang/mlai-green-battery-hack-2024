e5370b4c6ccd2bb6614f34b1ab32deb376865e60
BaoAL31 <jemboong@gmail.com>
2024-04-15 11:17:04 +1000

    submission MAs 40-50 expo 8-6 thres 1-2.5

--- bot/policies/moving_average - Copy.py
+++ bot/policies/moving_average - Copy.py
@@ -62,7 +62,7 @@ class VerySimplePolicy(Policy):
             solar_kW_to_battery = external_state['pv_power'] * (1 - self.exponential_increase(diff_percent, 8))
         else:
             charge_kW = internal_state['max_charge_rate'] * self.exponential_increase(diff_percent, 6)
-            charge_kW = 0 if charge_kW < 1 else charge_kW
+            charge_kW = 0 if charge_kW < 2.5 else charge_kW
             solar_kW_to_battery = external_state['pv_power']
 
         return solar_kW_to_battery, charge_kW
