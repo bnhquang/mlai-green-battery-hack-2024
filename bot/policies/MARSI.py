9df3aefb8072ff80351925194431fc2c6562cd41
BaoAL31 <jemboong@gmail.com>
2024-04-16 16:49:45 +1000

    submission MAs 60-70 expo 8-8 rsi 40

---
+++ bot/policies/MARSI.py
@@ -0,0 +1,70 @@
+import pandas as pd
+from collections import deque
+import numpy as np
+from policies.policy import Policy
+
+class MARSI(Policy):
+    def __init__(self, short_window_size=60, long_window_size=70, rsi_size=14, rsi_thres=30):
+        """
+        Constructor for the MovingAveragePolicy.
+
+        :param window_size: The number of past market prices to consider for the moving average (default: 5).
+        """
+        super().__init__()
+        self.short_window_size = short_window_size
+        self.long_window_size = long_window_size
+        self.rsi_size = rsi_size
+        self.rsi_thres = rsi_thres
+        self.historic_price = deque([45 for i in range(long_window_size+10)], maxlen=long_window_size)
+        # historical_data = pd.read_csv('./data/validation_data.csv')
+        # self.load_historical(historical_data[:-100])
+
+    """
+    
+    """
+
+    def act(self, external_state, internal_state):
+        market_price = external_state['price']
+        self.historic_price.append(market_price)
+        price_series = pd.Series(self.historic_price)
+        # print(price_series)
+        short_ma = price_series.rolling(window=self.short_window_size).mean().iloc[-1]
+        long_ma = price_series.rolling(window=self.long_window_size).mean().iloc[-1]
+        rsi = self.calculate_rsi(self.historic_price)
+        # moving_average = np.mean(self.historic_price)
+        diff_percent = abs(abs(short_ma - long_ma) / ((short_ma + long_ma) / 2))
+        # print(f'Market price: {market_price}, rsi: {rsi}')
+        # print('Diff:', diff_percent)
+        if short_ma > long_ma:
+            charge_kW = -internal_state['max_charge_rate'] * self.exponential_increase(diff_percent, 8)
+            charge_kW = 0 if charge_kW >= self.rsi_thres else charge_kW
+            solar_kW_to_battery = external_state['pv_power'] * (1 - self.exponential_increase(diff_percent, 8))
+        else:
+            charge_kW = internal_state['max_charge_rate'] * self.exponential_increase(diff_percent, 8)
+            charge_kW = 0 if charge_kW < self.rsi_thres else charge_kW
+            solar_kW_to_battery = external_state['pv_power']
+
+        return solar_kW_to_battery, charge_kW
+
+    def calculate_rsi(self, prices):
+        deltas = np.diff(prices)
+        gains = deltas[deltas > 0]
+        losses = -deltas[deltas < 0]
+
+        avg_gain = np.mean(gains[-self.rsi_size:])
+        avg_loss = np.mean(losses[-self.rsi_size:])
+
+        rs = avg_gain / avg_loss
+        rsi = 100 - (100 / (1 + rs))
+        return rsi
+
+    def exponential_increase(self, num, factor):
+        return 1 - np.exp(-factor * num)
+
+    # Test with different input values
+
+    def load_historical(self, external_states: pd.DataFrame):
+        for price in external_states['price'].values:
+            self.historic_price.append(price)
+
+
--- bot/policies/MAs&ManualThres.py
+++ bot/policies/MAs&ManualThres.py
@@ -11,22 +11,13 @@ class VerySimplePolicy(Policy):
         :param window_size: The number of past market prices to consider for the moving average (default: 5).
         """
         super().__init__()
-        self.short = deque([45 for i in range(short_window_size)], maxlen=short_window_size)
-        self.long = deque([45 for i in range(long_window_size)], maxlen=long_window_size)
+        self.short = deque([30 for i in range(short_window_size)], maxlen=short_window_size)
+        self.long = deque([30 for i in range(long_window_size)], maxlen=long_window_size)
         # self.historic_price = deque([45 for i in range(historical_price_len)], maxlen=historical_price_len)
         # historical_data = pd.read_csv('/data/validation_data.csv')
         # self.load_historical(historical_data[:-100])
 
     """
-    2.79 6-12 with short percent
-    2.59 5-10 with short percent
-    3.08 8-16 with short
-    3.97 10-20 with long
-    4.95 10-20 diff * 2
-    4.56 10-20 expo 5
-    5.61 15-30 expo 5
-    8.06 20-40 expo 2
-    9.22 30-40 expo 5
     10.69 40-50 expo 6
     11.75 50-60 expo 7
     12.80 60-70 expo 8
@@ -45,6 +36,7 @@ class VerySimplePolicy(Policy):
     16.8 40-50 expo 8-8 thres 1-2.
     19.18 60-70 expo 8-8 thres 1-2
     18.73 60-70 expo 8-8 thres 0.5-1.5
+    21.22 60-70 expo 8-8 thres 0.5-2
     """
 
     def act(self, external_state, internal_state):
