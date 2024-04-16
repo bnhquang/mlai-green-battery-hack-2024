23a1e77b235d01b0c66dc2189f66143eeee07b7c
BaoAL31 <jemboong@gmail.com>
2024-04-16 18:16:42 +1000

    submission MAs 60-70 expo 8-4 rsi size 14 thres 50

--- bot/policies/MARSI.py
+++ bot/policies/MARSI.py
@@ -4,7 +4,7 @@ import numpy as np
 from policies.policy import Policy
 
 class MARSI(Policy):
-    def __init__(self, short_window_size=60, long_window_size=70, rsi_size=14, rsi_thres=40):
+    def __init__(self, short_window_size=60, long_window_size=70, rsi_size=14, rsi_thres=50):
         """
         Constructor for the MovingAveragePolicy.
 
