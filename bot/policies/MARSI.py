6ec8016df6327044034c160424067a184bc7f7d2
BaoAL31 <jemboong@gmail.com>
2024-04-16 22:34:44 +1000

    submission MARSI short_window_size=60, long_window_size=70, rsi_size=14, rsi_thres=(30, 74)

--- bot/policies/MARSI.py
+++ bot/policies/MARSI.py
@@ -4,7 +4,7 @@ import numpy as np
 from policies.policy import Policy
 
 class MARSI(Policy):
-    def __init__(self, short_window_size=60, long_window_size=70, rsi_size=14, rsi_thres=(30, 78)):
+    def __init__(self, short_window_size=60, long_window_size=70, rsi_size=14, rsi_thres=(30, 74)):
         """
         Constructor for the MovingAveragePolicy.
 
@@ -25,6 +25,8 @@ class MARSI(Policy):
     14.64 short_window_size=60, long_window_size=70, rsi_size=5, rsi_thres=(50, 40)
     15.30 short_window_size=60, long_window_size=70, rsi_size=10, rsi_thres=(50, 70)
     21.93 short_window_size=60, long_window_size=70, rsi_size=14, rsi_thres=(40, 80)
+    22.73 short_window_size=60, long_window_size=70, rsi_size=14, rsi_thres=(30, 78)
+     short_window_size=60, long_window_size=70, rsi_size=14, rsi_thres=(30, 74)
     """
 
     def act(self, external_state, internal_state):
