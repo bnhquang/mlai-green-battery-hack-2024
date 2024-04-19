be9ae51e5e76526a6cee195c5e2399c264317972
BaoAL31 <jemboong@gmail.com>
2024-04-19 13:15:27 +1000

    submission BB window_size=144, num_std_dev=2

--- bot/policies/BollingerBands.py
+++ bot/policies/BollingerBands.py
@@ -4,11 +4,11 @@ from collections import deque
 from policies.policy import Policy
 
 class BollingerBandsPolicy(Policy):
-    def __init__(self, window_size=20, num_std_dev=2):
+    def __init__(self, window_size=144, num_std_dev=2):
         super().__init__()
         self.window_size = window_size
         self.num_std_dev = num_std_dev
-        self.prices = deque(maxlen=window_size)
+        self.prices = deque([45 for i in range(window_size)], maxlen=window_size)
 
     def act(self, external_state, internal_state):
         current_price = external_state['price']
@@ -16,33 +16,27 @@ class BollingerBandsPolicy(Policy):
         max_charge_rate = internal_state['max_charge_rate']
 
         self.prices.append(current_price)
-        if len(self.prices) >= self.window_size:
-            avg = np.mean(self.prices)
-            std_dev = np.std(self.prices)
-            upper_band = avg + (std_dev * self.num_std_dev)
-            lower_band = avg - (std_dev * self.num_std_dev)
-            
-            diff_percent = abs(current_price - avg) / avg
+        price_series = pd.Series(self.prices)
+        avg = price_series.rolling(window=self.window_size).mean().iloc[-1]
+        std_dev = np.std(self.prices)
+        upper_band = avg + (std_dev * self.num_std_dev)
+        lower_band = avg - (std_dev * self.num_std_dev)
 
-            if current_price > upper_band:
-                charge_kW = -max_charge_rate * self.exponential_increase(diff_percent, 8)
-                charge_kW = 0 if charge_kW > -0.5 else charge_kW
-                solar_kW_to_battery = pv_power * (1 - self.exponential_increase(diff_percent, 8))
-                
-            elif current_price < lower_band:
-                charge_kW = max_charge_rate * self.exponential_increase(diff_percent, 8)
-                charge_kW = 0 if charge_kW < 2 else charge_kW
-                solar_kW_to_battery = pv_power * self.exponential_increase(diff_percent, 8)
-                
-            else:
-                solar_kW_to_battery = pv_power / 2
-                charge_kW = min(max_charge_rate, pv_power / 2)
+        diff_percent = abs(abs(current_price - avg) / ((avg + current_price) / 2))
+        # print(diff_percent)
+
+        if current_price > upper_band:
+            charge_kW = -max_charge_rate * self.exponential_increase(diff_percent, 8)
+            solar_kW_to_battery = pv_power * (1 - self.exponential_increase(diff_percent, 8))
+        elif current_price < lower_band:
+            charge_kW = max_charge_rate * self.exponential_increase(diff_percent, 1)
+            solar_kW_to_battery = pv_power * self.exponential_increase(diff_percent, 1)
         else:
-            # default action
-            solar_kW_to_battery = pv_power / 2
-            charge_from_grid = 0
+            charge_kW = 0
+            solar_kW_to_battery = pv_power
 
-        return solar_kW_to_battery, charge_from_grid
+
+        return solar_kW_to_battery, charge_kW
 
     def exponential_increase(self, num, factor):
         """
@@ -55,5 +49,5 @@ class BollingerBandsPolicy(Policy):
         return 1 - np.exp(-factor * num)
 
     def load_historical(self, external_states):
-        for state in external_states:
-            self.prices.append(state['price'])
+        for price in external_states['price'].values:
+            self.prices.append(price)
