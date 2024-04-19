e8913c294cd2ef287023a8fe405494c2d739c046
bnhquang <bnhquang127@gmail.com>
2024-04-19 12:52:02 +1000

    adjusted bollinger

--- bot/policies/BollingerBands.py
+++ bot/policies/BollingerBands.py
@@ -21,24 +21,38 @@ class BollingerBandsPolicy(Policy):
             std_dev = np.std(self.prices)
             upper_band = avg + (std_dev * self.num_std_dev)
             lower_band = avg - (std_dev * self.num_std_dev)
+            
+            diff_percent = abs(current_price - avg) / avg
 
             if current_price > upper_band:
-                solar_kW_to_battery = 0
-                charge_from_grid = 0
+                charge_kW = -max_charge_rate * self.exponential_increase(diff_percent, 8)
+                charge_kW = 0 if charge_kW > -0.5 else charge_kW
+                solar_kW_to_battery = pv_power * (1 - self.exponential_increase(diff_percent, 8))
                 
             elif current_price < lower_band:
-                solar_kW_to_battery = pv_power
-                charge_from_grid = min(max_charge_rate, pv_power)
+                charge_kW = max_charge_rate * self.exponential_increase(diff_percent, 8)
+                charge_kW = 0 if charge_kW < 2 else charge_kW
+                solar_kW_to_battery = pv_power * self.exponential_increase(diff_percent, 8)
                 
             else:
                 solar_kW_to_battery = pv_power / 2
-                charge_from_grid = min(max_charge_rate, pv_power / 2)
+                charge_kW = min(max_charge_rate, pv_power / 2)
         else:
             # default action
             solar_kW_to_battery = pv_power / 2
             charge_from_grid = 0
 
         return solar_kW_to_battery, charge_from_grid
+
+    def exponential_increase(self, num, factor):
+        """
+        Helper function to compute an exponential increase based on the difference percentage.
+
+        :param num: The difference percentage.
+        :param factor: The factor to influence the growth rate.
+        :return: The exponential growth result.
+        """
+        return 1 - np.exp(-factor * num)
 
     def load_historical(self, external_states):
         for state in external_states:
